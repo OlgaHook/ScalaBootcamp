092ee6c98deccfc2add9ccb7cd0615c2d0b1208d
Olga Hook <olga_krjuckova@inbox.lv>
2022-03-15 14:10:36 +0200

    StringTask

---
+++ src/main/scala/StringTask1.scala
@@ -0,0 +1,14 @@
+object StringTask1 {
+  //TODO
+  def processString(text:String, uppercaseChars:String ="", needsTrim:Boolean=false):String = {
+    //TODO first trim string if it needs trimming from the argument
+    //TODO replace All characters in uppercaseChars with their uppercase versions
+    //you will need to write a loop
+    //you will probably want to use var to store a temporary string that you keep reweriting
+    //return newly created string
+    "FIXME" //FIXME
+  }
+
+  println(processString("abracadabra", "cr")) //should print abRaCadabRa
+  println(processString("   abracadabra  ", "cr", needsTrim = true)) //should print abRaCadabRa
+}
