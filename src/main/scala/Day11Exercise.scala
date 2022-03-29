01ed4db77ef01c5ba1cbe0a751a1ff239c757332
Olga Hook <olga_krjuckova@inbox.lv>
2022-03-29 18:54:59 +0300

    Day11 exercise

--- src/main/scala/Day11Exercise.scala
+++ src/main/scala/Day11Exercise.scala
@@ -1,7 +1,28 @@
+import Day11Set.{mySet, sentence, uniqueCharSet}
+
 object Day11Exercise extends App {
   //TODO write a pangram tester
   //https://en.wikipedia.org/wiki/Pangram
   //check if text contains every letter of an alphabet at least once
+  val charSet = "abcdefghijklmnopqrstuvwxyz".toSet
+  println(charSet)
+  val arr = charSet.toArray.sorted
+  println(arr.mkString(","))
+  for (n <- arr) {
+    println(s"Item $n is in my set")
+  }
+/*
+def isPangram(text:String, alpha:String="abcdefghijklmnopqrstuvwxyz"):Boolean = {
+    val charSet = text.toLowerCase.toSet
+    //println(charSet)
+    val alphaSet = alpha.toSet
+    //println(alphaSet)
+    val check = charSet & alphaSet
+    //println(check)
+    check.subsetOf(alphaSet)
+  }
+ */
+
   //for this task consider upuper and lower case the same
   def isPangram(text:String, alpha:String="abcdefghijklmnopqrstuvwxyz"):Boolean = {
 
