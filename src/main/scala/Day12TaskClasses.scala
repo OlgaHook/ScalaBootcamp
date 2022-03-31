57567cb30cabe28b2a481b6448b9f31e32a77c82
Olga Hook <olga_krjuckova@inbox.lv>
2022-03-31 14:20:33 +0300

    Day12Task

---
+++ src/main/scala/Day12TaskClasses.scala
@@ -0,0 +1,18 @@
+
+//TODO add class variables name, animalType, likes, sound- all strings and age which is Int
+//class Animal(  ) {
+//  //TODO add class method makeSound which prints sound
+//
+//  //BONUS
+//  //TODO add class method meet which has a parameter of contact:String
+//  //TODO this meet should decide whether to greet if they like the contact
+//  //TODO to run away if they do not like the contact
+//
+//
+//}
+
+object Day12TaskClasses extends App {
+  //TODO create some animals objects from Animal class with appropriate properties, dog, cat, mouse etc :)
+  //TODO call makeSound method
+  //TODO call meet method
+}
