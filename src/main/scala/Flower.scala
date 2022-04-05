2eafabed49b487e9a3090c9b8f03f925f4718a31
Olga Hook <olga_krjuckova@inbox.lv>
2022-04-05 10:18:50 +0300

    Flower class

---
+++ src/main/scala/Flower.scala
@@ -0,0 +1,5 @@
+class Flower(flowerType:String, val color:String) {
+  println(s"Created a new flower of type: $flowerType with color $color")
+  //more methods here
+  //more construction work here
+}
