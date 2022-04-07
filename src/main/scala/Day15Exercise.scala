b602168fd0e1543b5f063efa10067f2ecea6ba4a
Olga Hook <olga_krjuckova@inbox.lv>
2022-04-07 18:24:49 +0300

    Exercise

---
+++ src/main/scala/Day15Exercise.scala
@@ -0,0 +1,13 @@
+object Day15Exercise extends App {
+  //TODO write a function getDay(day: Int): String that returns day of the week in English Monday, Tuesday and so on until Sunday
+  //Given an index 1 to 7
+  //if value is outside you return Unknown Weekday
+
+
+  //TODO write a function getDayType(day: String): String
+  // that returns "Workday" for "Monday" to "Friday" and "Weekend" for "Saturday" and "Sunday"
+  // default can be "Groundhog Day" or something equally silly
+
+  //TODO test the functions with some data!
+}
+
