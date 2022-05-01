c52da427476e6b8399c378c47fc9d283bd93ba1a
Olga Hook <olga_krjuckova@inbox.lv>
2022-05-01 17:55:29 +0300

    Day21 JSON 2nd part

--- src/main/scala/com/github/OlgaHook/Day21ReadingJSONwithUPickle.scala
+++ src/main/scala/com/github/OlgaHook/Day21ReadingJSONwithUPickle.scala
@@ -1,6 +1,6 @@
 package com.github.OlgaHook
 
-object Day21ReadingJSONwithUPickle extends App{
+object Day21ReadingJSONwithUPickle extends App {
   val src = "src/resources/json/fruitFacts.json"
 
   val rawText = MyUtil.getTextFromFile(src)
@@ -21,19 +21,53 @@ object Day21ReadingJSONwithUPickle extends App{
       o("id").num.toInt,
       o("family").str,
       o("order").str,
-      o("nutritions")("carbohydrates").num //so Double by default
+      o("nutritions")("carbohydrates").num, //so Double by default
       //TODO add the rest of the fields proteins etc
+      o("nutritions")("sugar").num,
+      o("nutritions")("protein").num,
+      o("nutritions")("fat").num,
+      o("nutritions")("calories").num
     )
   }
 
   fruits.take(3).foreach(println)
+  
+  val highCalFruits = fruits.sortBy(_.calories)
+
+  println(s"The highest calories per 100g is ${highCalFruits.reverse.head} ")
 
   //TODO find most calorie dense fruit - it looks the data is per 100grams maybe someone can verify this?
   //TODO find top 5 fattiest fruits
+  val topFatFruits = fruits.sortBy(_.fat)
+  println(s"The highest fat content per 100g: ")
+  topFatFruits.reverse.take(5).foreach(println) //here it is println(_)
+  topFatFruits.reverse.take(5).foreach(_.prettyPrint())
+
   //TODO find top 5 protein sources for fruits
+  //TODO find top 5 protein sources for fruits
+  val topProteinFruits = fruits.sortBy(_.protein)
+  println(s"The highest protein content per 100g: ")
+  topProteinFruits.reverse.take(5).foreach(_.prettyPrint())
   //TODO find 5 least sugary fruits
+  val minSugarFruits = fruits.sortBy(_.sugar)
+  println(s"The lowest sugar  content per 100g: ")
+  minSugarFruits.take(5).foreach(_.prettyPrint())
+
   //TODO find 5 fruits with most carbohydrates that are not sugars (so difference between carbohydrates and sugar)
 
   //you can add some extra conclusions, statistics as well
+  val nonSugarCarbs = fruits.sortBy(_.nonSugarCarbs)
+  println(s"The highest non sugar carbohydrates per 100g: ")
+  nonSugarCarbs.reverse.take(5).foreach(_.prettyPrint())
 
+  //we create a JSON string without a library
+  val jsonString = "[" + fruits.map(_.getJSON()).mkString(",\n") + "]"
+
+  val dst = "src/resources/json/fruits.json"
+  MyUtil.saveText(dst, jsonString)
+
+  val jsonText = upickle.default.write(Seq(1, 2, 3))
+  println(jsonText)
+
+  //TODO use the upickle library to write the Case class to JSON without doing it ourselves
 }
--- src/main/scala/com/github/OlgaHook/Fruit.scala
+++ src/main/scala/com/github/OlgaHook/Fruit.scala
@@ -6,6 +6,30 @@ case class Fruit(genus: String,
                  family: String,
                  order: String,
                  //i am flattening the nutritions
-                 carbohydrates: Double = 0.0
-//TODO add the rest of fields from nutriotions
-)
+                 carbohydrates: Double = 0.0,
+                 sugar:Double = 0.0,
+                 protein:Double = 0.0,
+                 fat:Double = 0.0,
+                 calories:Double = 0.0 )
+{val nonSugarCarbs : Double = carbohydrates - sugar
+
+  def prettyPrint():Unit = {
+    println(s"Fruit genus:$genus, name: $name, family:$family, order:$order\n" +
+      s", carbs:$carbohydrates, sugar:$sugar, protein: $protein, fat:$fat, calories:$calories, nonSugarCarbs:$nonSugarCarbs")
+}
+  def getJSON():String = {
+    s"""
+       |{
+       |  "genus": "$genus",
+       |  "name": "$name",
+       |  "family": "$family",
+       |  "order": "$order",
+       |  "carbohydrates": "$carbohydrates",
+       |  "sugar": "$sugar",
+       |  "protein": "$protein",
+       |  "fat": "$fat",
+       |  "calories": "$calories"
+       |}
+       |""".stripMargin
+  }
+}
